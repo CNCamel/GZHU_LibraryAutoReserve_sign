0722b4384cbef0b71fdf0fd4e575306a22efc692
CNCamel <112789873+CNCamel@users.noreply.github.com>
2024-02-28 00:11:03 +0800

    Update reserve.py

--- reserve.py
+++ reserve.py
@@ -3,14 +3,14 @@
 """
 from libs.info import infos
 from libs.source import ZWYT
-
+import os
 
 def main(*args, **kwargs):
     # 遍历 info 信息，获取每个用户的昵称、预约座位号、用户名、密码、时间段、推送token（推送可以为空）
     for stu in infos:
         try:
             # 初始化类示例，传入昵称、用户名、密码、时间段、推送token（推送可以为空）
-            yy = ZWYT(stu['name'], stu['sno'], stu['pwd'], stu['periods'], stu['pushplus'])
+            yy = ZWYT(stu['name'], stu[str(os.environ['XUHAO'])], stu[str(os.environ['MIMA'])], stu['periods'], stu['pushplus'])
 
             # 调用预约函数预约，传入预约座位号
             yy.reserve(stu['devName'])
