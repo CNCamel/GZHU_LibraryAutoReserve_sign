e0b6044054d4e204f0d377dd075624b220fca889
CNCamel <112789873+CNCamel@users.noreply.github.com>
2024-03-27 23:30:24 +0800

    Update info.py

--- libs/info.py
+++ libs/info.py
@@ -17,7 +17,7 @@ infos = [
     {
         'sno': str(os.environ['XUHAO1']),
         'pwd': str(os.environ['MIMA1']),
-        'devName': '3c-110',
+        'devName': '303-015',
         'name': '皮卡丘',
         'periods': (
             ('8:30:00', '12:30:00'),
